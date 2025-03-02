35519456dd97b35ec30e4c6c54521d46d47408af
Arif <aarifokcan@gmail.com>
2025-03-02 04:24:41 +0300

    Modify read_data()

--- src/read_data.c
+++ src/read_data.c
@@ -13,16 +13,52 @@
 #include "../inc/fdf.h"
 #include "../lib/libft/libft.h"
 #include "../lib/get_next_line/get_next_line.h"
+#include <stdlib.h>
 #include <unistd.h>
 
-static	unsigned int	nl_counter(int fd)
+static unsigned int	nl_counter(int fd)
 {
+	char			temp[READ_BUFFER_SIZE];
+	unsigned int	nl_count;
+	unsigned int	i;
+	int				bytes_read;
 
+	nl_count = 0;
+	bytes_read = read(fd, temp, READ_BUFFER_SIZE);
+	while (bytes_read > 0)
+	{
+		i = 0;
+		while (i < bytes_read)
+		{
+			if (temp[i] == '\n')
+				nl_count++;
+			i++;
+		}
+		bytes_read = read(fd, temp, READ_BUFFER_SIZE);
+	}
+	return (nl_count);
+}
+
+static int	insert_line(char *str)
+{
+	return (0);
 }
 
 void	read_data(t_data *data, int fd)
 {
 	unsigned int	new_line;
+	unsigned int	i;
 
 	new_line = nl_counter(fd);
+	if (new_line > 0)
+	{
+		data->pos_info = malloc(sizeof(char *) * (new_line + 1));
+		data->pos_info[new_line] = NULL;
+	}
+	i = 0;
+	while (data->pos_info[i] != NULL)
+	{
+		data->pos_info[i] = get_next_line(fd);
+		i++;
+	}
 }
