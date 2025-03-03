b95873dc407d216a4293728aee3b999c65958c46
Arif <aarifokcan@gmail.com>
2025-03-04 02:42:09 +0300

    Create init.c file

--- src/init.c
+++ src/init.c
@@ -5,17 +5,9 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: muokcan <muokcan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/28 18:19:06 by muokcan           #+#    #+#             */
-/*   Updated: 2025/02/28 18:52:31 by muokcan          ###   ########.fr       */
+/*   Created: 2025/03/04 02:35:26 by muokcan           #+#    #+#             */
+/*   Updated: 2025/03/04 02:39:06 by muokcan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/fdf.h"
-#include "../lib/libft/libft.h"
-
-int	main(int argc, char **argv)
-{
-	t_data	data;
-
-	read_data(&data, ft_atoi(argv[1]));
-}
+void	init();
