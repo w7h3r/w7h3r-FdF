13ed650b0df585bf3cb6f2093703853e78c38e30
Muhammet Arif Okcan <muokcan@k2m04s06.42kocaeli.com.tr>
2025-03-05 17:27:28 +0300

    Modify files

--- src/read_data.c
+++ src/read_data.c
@@ -36,19 +36,43 @@ int	count_line(int fd)
 	return (nl);
 }
 
-void	insert_lines(t_data *data, int fd, int nl, int line_)
+char	*insert_cell(char *str, _Bool i)
+{
+
+}
+
+int		count_space(char *str)
+{
+	int	space_count;
+
+	space_count = 0;
+	while (*str)
+	{
+		if (*str == ' ')
+			space_count++;
+		str++;
+	}
+	return (space_count);
+}
+
+void	insert_lines(t_data *data, int fd, int line_)
 {
 	char	**temp;
 	char	*line;
 	int		i;
+	int		spc;
 
 	line = get_next_line(fd);
 	temp = ft_split(line, ' ');
+	spc = count_space(line);
 	free(line);
 	i = 0;
 	while (temp[i])
 	{
-		data->map.cells[line_][i].value = ft_atoi(temp[i]);
+		data->map.cells[line_] = malloc(sizeof(int *) * (spc + 1));
+		data->map.cells[line_][i].value = ft_atoi(insert_cell(temp[i], 0));
+		data->map.cells[line_][i].color = ft_atoi(insert_cell(temp[i], 1));
+		i++;
 	}
 }
 
@@ -65,7 +89,7 @@ void	read_map(t_data *map, const char *file)
 	i = 0;
 	while (i < nl)
 	{
-		insert_lines(map, fd, nl, i);
+		insert_lines(map, fd, i);
 		i++;
 	}
 }
