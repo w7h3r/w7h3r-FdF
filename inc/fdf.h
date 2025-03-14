dac3f1a42fea5d3757f28e5942938a218dab01f5
Arif <aarifokcan@gmail.com>
2025-03-14 05:24:24 +0300

    Add mlx & position structs

--- inc/fdf.h
+++ inc/fdf.h
@@ -6,7 +6,7 @@
 /*   By: muokcan <muokcan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 18:16:00 by muokcan           #+#    #+#             */
-/*   Updated: 2025/03/10 16:30:11 by muokcan          ###   ########.fr       */
+/*   Updated: 2025/03/14 01:35:48 by muokcan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@
 # define READ_BUFFER_SIZE 512
 #endif
 
-# define WIN_H 1800
-# define WIN_W 1200
+# define WIN_H 720
+# define WIN_W 1280
 
 typedef struct s_cell {
 	int	value;
@@ -29,12 +29,30 @@ typedef struct s_map {
 	t_cell	**inf;
 	int		x;
 	int		y;
+	int		scale;
 }	t_map;
+
+typedef struct s_pos {
+	int	x0;
+	int	y0;
+	int	x1;
+	int	y1;
+}	t_pos;
+
+typedef struct s_mlx {
+	void	*mlx;
+	void	*win;
+	void	*img;
+	char	*addr;
+	int		bitbp;
+	int		l_len;
+	int		endian;
+}	t_mlx;
 
 typedef struct s_data {
 	t_map	map;
-	void	*mlx;
-	void	*win;
+	t_pos	p_pos;
+	t_mlx	mlx;
 }	t_data;
 
 void	read_data(t_data *data, const char *file);
@@ -42,5 +60,6 @@ void	read_data(t_data *data, const char *file);
 int		count_line(const char *file);
 int		count_space(const char *file);
 void	free_double(char **str0, char *str1);
+void	free_map(t_map *map);
 
 #endif
