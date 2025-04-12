79b32e119de1004e17fac9faf9482ace7fecd0df
Arif <aarifokcan@gmail.com>
2025-04-12 21:20:58 +0300

    Update init.c

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: muokcan <muokcan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 02:35:26 by muokcan           #+#    #+#             */
-/*   Updated: 2025/04/11 02:42:41 by muokcan          ###   ########.fr       */
+/*   Updated: 2025/04/12 18:22:33 by muokcan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,12 @@
 
 void	free_data(t_data *data)
 {
-	mlx_destroy_image(data->mlx.mlx, data->mlx.img);
-	mlx_destroy_window(data->mlx.mlx, data->mlx.win);
+	if (data->mlx.img)
+		mlx_destroy_image(data->mlx.mlx, data->mlx.img);
+	if (data->mlx.win)
+		mlx_destroy_window(data->mlx.mlx, data->mlx.win);
+	if (data->mlx.mlx)
+		mlx_destroy_display(data->mlx.mlx);
 	free_map(&data->map);
 }
 
@@ -31,18 +35,17 @@ void	err_exit_init(t_data *data, const char *err_msg)
 
 void	init(t_data *data)
 {
-    data->mlx.mlx = mlx_init();
-    if (!data->mlx.mlx)
+	data->mlx.mlx = mlx_init();
+	if (!data->mlx.mlx)
 		err_exit_init(data, "Error: Can't initialize mlx.");
 	data->mlx.win = mlx_new_window(data->mlx.mlx, W_WI, W_HE, "FdF");
-    if (!data->mlx.win)
+	if (!data->mlx.win)
 		err_exit_init(data, "Error: Can't initialize window");
-   	data->mlx.img = mlx_new_image(data->mlx.mlx, W_WI, W_HE);
-    if (!data->mlx.img)
+	data->mlx.img = mlx_new_image(data->mlx.mlx, W_WI, W_HE);
+	if (!data->mlx.img)
 		err_exit_init(data, "Error: Can't initialize image");
 	data->mlx.addr = mlx_get_data_addr(data->mlx.img, &data->mlx.bitbp, \
 			&data->mlx.l_len, &data->mlx.endian);
-    if (!data->mlx.addr)
+	if (!data->mlx.addr)
 		err_exit_init(data, "Error: Can't take the image addr");
 }
-
