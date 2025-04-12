4cfb93ebd88ad08cad5a7022c0391ee14ee0c53d
Arif <aarifokcan@gmail.com>
2025-04-12 21:19:59 +0300

    Approve: draw works

--- src/draw.c
+++ src/draw.c
@@ -6,25 +6,20 @@
 /*   By: muokcan <muokcan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 22:58:24 by muokcan           #+#    #+#             */
-/*   Updated: 2025/04/11 02:33:10 by muokcan          ###   ########.fr       */
+/*   Updated: 2025/04/12 21:19:26 by muokcan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/fdf.h"
-#include "../lib/minilibx/mlx.h"
-#include "../lib/ft_printf/include/ft_printf.h"
 
-/*
- * draw_line() is a re-implementation of bressenham's line algorithm to this project.
- */
-
-void put_pixel_to_img(t_data *data, int x, int y, int color)
+void	put_pixel_to_img(t_data *data, int x, int y, int color)
 {
-    int *pixel;
+    int	*pixel;
 
     if (x < 0 || y < 0 || x >= W_WI || y >= W_HE)
-		return;
-	pixel = (int *)(data->mlx.addr + (y * data->mlx.l_len + x * (data->mlx.bitbp / 8)));
+		return ;
+	pixel = (int *)(data->mlx.addr + \
+			(y * data->mlx.l_len + x * (data->mlx.bitbp / 8)));
 	*pixel = color;
 }
 
@@ -41,30 +36,61 @@ static void	draw_line_controls(t_line *line, t_pos *pos)
 	line->err = line->dx - line->dy;
 }
 
-void	draw_line(t_data *data, t_pos pos)
+static void	line_control(t_pos *pos, t_line *line)
 {
-	t_line line;
+		line->e2 = 2 * line->err;
+		if (line->e2 > -line->dy)
+		{
+			line->err -= line->dy;
+			pos->x0 += line->sx;
+		}
+		if (line->e2 < line->dx)
+		{
+			line->err += line->dx;
+			pos->y0 += line->sy;
+		}
+}
 
+static void	draw_line(t_data *data, t_pos pos, int f_color, int s_color)
+{
+	t_line	line;
+	t_color	clr;
+
+	clr.curr_step = 0;
 	line.dx = abs(pos.x1 - pos.x0);
 	line.dy = abs(pos.y1 - pos.y0);
+	clr.steps = line.dy;
+	if (line.dx > line.dy)
+		clr.steps = line.dx;
 	draw_line_controls(&line, &pos);
 	while (1)
-	{   
-		//ft_printf("Put pixel at (%d, %d)\n", pos.x0, pos.y0);
-		put_pixel_to_img(data, pos.x0, pos.y0, 0xFFFFFF);
+	{
+		set_color(&clr);
+		if (f_color == 0)
+			clr.color = 0xFFFFFF;
+		else
+			clr.color = color_inter(clr.ratio, f_color, s_color);
+		put_pixel_to_img(data, pos.x0, pos.y0, clr.color);
 		if (pos.x0 == pos.x1 && pos.y0 == pos.y1)
 			break ;
-		line.e2 = 2 * line.err;
-		if (line.e2 > -line.dy)
-		{
-			line.err -= line.dy;
-			pos.x0 += line.sx;
-		}
-		if (line.e2 < line.dx)
-		{
-			line.err += line.dx;
-			pos.y0 += line.sy;
-		}
+		line_control(&pos, &line);
+		clr.curr_step++;
+	}
+}
+
+static void	draw_hv_lines(t_data *data, t_pos *pos, int x, int y)
+{
+	if (x < data->map.x - 1)
+	{
+		*pos = isometric_points(data->map.inf[y][x], \
+				data->map.inf[y][x + 1], data->map.z_scale);
+		draw_line(data, *pos, data->map.inf[y][x].color, data->map.inf[y][x + 1].color);
+	}
+	if (y < data->map.y - 1)
+	{
+		*pos = isometric_points(data->map.inf[y][x], \
+				data->map.inf[y + 1][x], data->map.z_scale);
+		draw_line(data, *pos, data->map.inf[y][x].color, data->map.inf[y + 1][x].color);
 	}
 }
 
@@ -80,16 +106,7 @@ void	draw_map(t_data *data)
 		x = 0;
 		while (x < data->map.x)
 		{
-			if (x < data->map.x - 1)
-			{
-				pos = isometric_points(data->map.inf[y][x], data->map.inf[y][x + 1]);
-				draw_line(data, pos);
-			}
-			if (y < data->map.y - 1)
-			{
-				pos = isometric_points(data->map.inf[y][x], data->map.inf[y + 1][x]);
-				draw_line(data, pos);
-			}
+			draw_hv_lines(data, &pos, x, y);
 			x++;
 		}
 		y++;
