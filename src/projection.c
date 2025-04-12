229d08fc00a170ac9689ab1d5ca505955b26f0e3
Arif <aarifokcan@gmail.com>
2025-04-12 21:22:07 +0300

    Back-up commit. Not finished

--- src/projection.c
+++ src/projection.c
@@ -6,7 +6,7 @@
 /*   By: muokcan <muokcan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 23:08:45 by muokcan           #+#    #+#             */
-/*   Updated: 2025/04/11 01:22:06 by muokcan          ###   ########.fr       */
+/*   Updated: 2025/04/12 20:48:48 by muokcan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,32 +22,58 @@
  * Multiply the x and y points positions as scale, apply isometric projection to points
  * and shift the map through the center of the window.
  */
-
-static void	apply_isometric_to_point(int *x, int *y, int z)
+static void	apply_isometric_to_point(int *x, int *y, int z, int z_scale)
 {
 	int	base_x;
 	int	base_y;
 
+	(void)z_scale;
 	base_x = *x;
 	base_y = *y;
-	*x = (base_x - base_y) * COS_30;
-	*y = (base_x + base_y) * SIN_30 - z;
+	*x = (base_x - base_y) * H_FACTOR;
+	*y = (base_x + base_y) * V_FACTOR - z;
 }
 
-t_pos	isometric_points(t_cell first, t_cell second)
+void	calculate_z_scale(t_map *map)
 {
-	t_pos pos;
+	int	col;
+	int	row;
+	int	min;
+	int	max;
+
+	min = map->inf[0][0].value;
+	max = map->inf[0][0].value;
+	row = 0;
+	while (row < map->y)
+	{
+		col = 0;
+		while (col < map->x)
+		{
+			if (min > map->inf[row][col].value)
+				min = map->inf[row][col].value;
+			if (max < map->inf[row][col].value)
+				max = map->inf[row][col].value;
+			col++;
+		}
+		row++;
+	}
+	map->z_scale = max - min;
+}
+
+t_pos	isometric_points(t_cell first, t_cell second, int z_scale)
+{
+	t_pos	pos;
 
 	first.x *= SCALE;
 	first.y *= SCALE;
 	second.x *= SCALE;
 	second.y *= SCALE;
-	apply_isometric_to_point(&first.x, &first.y, first.value);
-	apply_isometric_to_point(&second.x, &second.y, second.value);
-	first.x += W_WI / 2 - (5 * SCALE);
-	first.y += W_HE / 2 - (5 * SCALE + 245);
-	second.x += W_WI / 2 - (5 * SCALE);
-	second.y += W_HE / 2 - (5 * SCALE + 245);
+	apply_isometric_to_point(&first.x, &first.y, first.value, z_scale);
+	apply_isometric_to_point(&second.x, &second.y, second.value, z_scale);
+	first.x += W_WI / 2;
+	first.y += W_HE / 2;
+	second.x += W_WI / 2;
+	second.y += W_HE / 2;
 	pos.x0 = first.x;
 	pos.y0 = first.y;
 	pos.z0 = first.value;
